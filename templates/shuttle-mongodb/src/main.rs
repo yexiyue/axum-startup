1c343141f6bf7a6f6dcb393f0330bf0cef06ff56
yexiyue <yexiyue666@qq.com>
2024-01-14 20:51:11 +0800

    feat: make shuttle-mongodb template

--- templates/shuttle-mongodb/src/main.rs
+++ templates/shuttle-mongodb/src/main.rs
@@ -1,13 +1,7 @@
-use axum::{routing::get, Router};
-use sqlx::PgPool;
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use mongodb::Database;
 
 #[shuttle_runtime::main]
-async fn main(#[shuttle_shared_db::Postgres] db: PgPool) -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
+async fn main(#[shuttle_shared_db::MongoDb] db: Database) -> shuttle_axum::ShuttleAxum {
+    let router=shuttle_mongodb::start(db).await;
     Ok(router.into())
 }
